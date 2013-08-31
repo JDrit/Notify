73d72b787e1cf8acceb5bb8dfbdecc237aa64e77
jd <jd@csh.rit.edu>
2013-08-31 12:05:29 -0400

    updated schema

---
+++ models.py
@@ -0,0 +1,121 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+
+class Carrier(db.Model):
+    """
+    This stores information about the various phone carrier providers.
+        carrier_name: the string of the name
+        domain: the domain used for emailing
+        users: the users who use this carrier
+    """
+    __tablename__ = 'cellCarrierInfo'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+    id = db.Column(db.Integer,  primary_key = True, autoincrement = True)
+    carrier_name = db.Column(db.Text, nullable = False)
+    domain = db.Column(db.Text, nullable = False)
+    users = db.relationship('User', backref='carrier')
+
+    def __init__(self, name, domain):
+        self.carrier_name = name
+        self.domain = domain
+
+class User(db.Model):
+    """
+    Holds notify users' information
+        uid_number: the uid number of the user as the primary key
+        phone_number: the number to use to text the user
+        carrier_id: the id of the carrier being used
+        subscriptions: the subscriptions that the user has
+        admin: if the user is an admin for notify
+    """
+    __tablename__ = 'users'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+    uid_number = db.Column(db.Integer, primary_key = True)
+    phone_number = db.Column(db.BigInteger)
+    carrier_id = db.Column(db.Integer,
+            db.ForeignKey('cellCarrierInfo.id', onupdate='cascade',
+                ondelete='cascade'))
+    subscriptions = db.relationship('Subscription',
+            backref='user')
+    admin = db.Column(db.Boolean)
+
+    def __init__(self, uid_number, phone_number = None, carrier = None, admin = False):
+        self.uid_number = uid_number
+        self.phone_number = phone_number
+        self.carrier_id = carrier
+        self.admin = admin
+
+class Service(db.Model):
+    """
+    The services that can use notify
+        api_key: the key used to authenticate the service to the server
+        service_name: the name of the service
+        owner: the uid number of the owner of the service
+        active: if the service is currently allowed to send notifications
+        subscriptions: the subscriptions being used for this service
+        logs: the logs that are about these logs
+    """
+    __tablename__ = 'services'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    api_key = db.Column(db.Text, nullable = False)
+    service_name = db.Column(db.Text, nullable = False)
+    owner = db.Column(db.Integer, nullable = False)
+    subscription_service = db.Column(db.Boolean)
+    active = db.Column(db.Boolean)
+    subscriptions = db.relationship('Subscription',
+            backref='service')
+    logs = db.relationship('Log', backref='service')
+
+    def __init__(self, api_key, name, owner, subs, active):
+        self.api_key = api_key
+        self.service_name = name
+        self.owner = owner
+        self.subscription_service = subs
+        self.active = active
+
+class Subscription(db.Model):
+    """
+    The subscriptions that users have to various serivce. This is used
+    to store the list of services that users are using
+        uid_number: the uid number of the user
+        service_id: the id of the service being subscribed to
+        state: 0 = send email, 1 = send text, 2 = send both
+    """
+    __tablename__ = 'subscriptions'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    uid_number = db.Column(db.Integer, db.ForeignKey(User.uid_number,
+        onupdate='cascade', ondelete='cascade'))
+    service_id = db.Column(db.Integer, db.ForeignKey(Service.id,
+        onupdate='cascade', ondelete='cascade'))
+    state = db.Column(db.Integer)
+
+    def __init__(self, user, service, state):
+        self.user = user
+        self.service = service
+        self.state = state
+
+class Log(db.Model):
+    """
+    Logs of all emails and text messages send
+        date: the datetime of the message being sent
+        service_id: the id of the service being used
+        uid_number: the uid number of the user who was sent the message
+        email: the address sent the message
+    """
+    __tablename__ = 'logs'
+    __table_args__ = {'mysql_engine': 'InnoDB'}
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    date = db.Column(db.DateTime, default = datetime.datetime.now())
+    service_id = db.Column(db.Integer, db.ForeignKey(Service.id,
+        onupdate='cascade'))
+    uid_number = db.Column(db.Integer, nullable = False)
+    email = db.Column(db.Text, nullable = False)
+
+    def __init__(self, service, user, email):
+        self.service = service
+        self.user = user
+        self.email = email
---
+++ server.py
@@ -0,0 +1,66 @@
+from flask import Flask, request
+import ConfigParser
+from mailer import mail
+from models import *
+
+config = ConfigParser.ConfigParser()
+config.read('config')
+uri = config.get('sql', 'uri')
+port = config.getint('main', 'port')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+db.init_app(app)
+
+
+def check_uid_number(uid_number):
+    return True
+
+def get_username(uid_number):
+    return "jd"
+
+@app.route("/notify", methods=['GET', 'POST'])
+def notify():
+    phone_email = csh_email = None
+
+    # checks for valid input in the GET request
+    uid_number = request.args.get("uid_number", "")
+    if uid_number == "":
+        return "no uid number"
+    notification = request.args.get("notification", "")
+    if notification == "":
+        return "no notification"
+    api_key = request.args.get("apiKey", "")
+    if api_key == "":
+        return "no api key"
+
+    service = Service.query().filter_by(api_key = api_key).first()
+    if not service:
+        return "invalid API key"
+    user = Service.query().filter_by(uid_number = uid_number).first()
+    if not user: # make a user entry for the new user
+        if check_uid_number(uid_number):
+            db.session.add(User(uid_number))
+        else:
+            return "invalid uid number"
+    subscription = Subscription.query().filter(
+            db.and_(Subscription.user == user.uid_number,
+                Subscription.service == service.id)).first()
+    if not subscription:
+        return "user not subscribed to service"
+
+    # sends the text message
+    if subscription.state == 1 or subscription.state == 2:
+        carrier = Carrier.query().filter_by(id = user.carrier).first()
+        phone_email = user.phone_number + "@" + carrier.domain
+        mail(phone_email, "CSH Notification - " + service.service_name, notification)
+        db.session.add(Log(service.id, user.uid_number, phone_email))
+    # sends the email to the csh account
+    if subscription.state == 0 or subscription.state == 2:
+        csh_email = get_username(user.uid_number) + "@csh.rit.edu"
+        mail(csh_email, "CSH Notification - " + service.service_name, notification)
+        db.session.add(Log(service.id, user.uid_number, csh_email))
+    db.session.commit()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port = port)
